051b825751cebdcd8338db2792fbd5f686fd1e52
Akunna Onyekachi <akunna1mail@gmail.com>
2025-02-15 14:25:07 -0500

    New favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/*Defining Favicon*/}
+      <head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
